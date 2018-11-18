2aaee62e0a5b5efca339abf8915bd0415aa73f0a
deeqkko <kimmo.koivusalo@student.hamk.fi>
2018-11-18 17:54:30 +0200

    Open, Save As, Cut, Copy, Paste are working

--- notpad/MainWindow.xaml.cs
+++ notpad/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace notpad
 {
@@ -23,6 +13,126 @@ namespace notpad
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        
+        int lineCount;
+        string documentFilename;
+     
+        //Read and write operations
+        private void WriteToFile(string documentFilename)
+        {
+            StreamWriter writer = File.CreateText(documentFilename);
+            int i = 0;
+            while (i < lineCount)
+            {
+                string lineText = Paper.GetLineText(i);
+                lineText = lineText.TrimEnd();
+                writer.WriteLine(lineText);
+                i++;
+                Console.WriteLine("INFO: Line number " + i + "wrote.");
+            }
+            writer.Close();
+        }
+
+        private void ReadFromFile(string documentFilename)
+        {
+            StreamReader reader = File.OpenText(documentFilename);
+            string documentLine = reader.ReadLine();
+            while (documentLine != null)
+            {
+                Paper.AppendText(documentLine + "\n");
+                documentLine = reader.ReadLine();
+            }
+            reader.Close();
+        }
+
+
+
+        //Menu functions
+        private void MenuItem_Open(object sender, RoutedEventArgs e)
+        {
+            
+            Microsoft.Win32.OpenFileDialog OpenDialog = new Microsoft.Win32.OpenFileDialog();
+            OpenDialog.FileName = "Document";
+            OpenDialog.DefaultExt = ".txt";
+            OpenDialog.Filter = "Text Document |*.txt";
+
+            bool? result = OpenDialog.ShowDialog();
+
+                if (result == true)
+                {
+                    Paper.Clear();
+                    documentFilename = OpenDialog.FileName;
+                    ReadFromFile(documentFilename);
+                }
+            
+        }
+
+        private void MenuItem_Print(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                printDialog.PrintVisual(Paper, "Tähän tiedostonimi");
+            }
+        }
+
+        private void MenuItem_Exit(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void MenuItem_SaveAs(object sender, RoutedEventArgs e)
+        {
+            var SaveAsDialog = new Microsoft.Win32.SaveFileDialog
+            {
+                FileName = "My TextDocument.txt",
+                DefaultExt = ".txt",
+                Filter = "Text Document |*.txt"
+            };
+
+            bool? result = SaveAsDialog.ShowDialog();
+
+            if (result == true)
+            {
+                int lineCount = Paper.LineCount;
+                documentFilename = SaveAsDialog.FileName;
+                WriteToFile(documentFilename);
+            }
+        }
+
+        private void MenuItem_Close(object sender, RoutedEventArgs e)
+        {
+            Paper.Clear();
+        }
+
+        private void MenuItem_Copy(object sender, EventArgs e)
+        {
+            if (Paper.SelectionLength > 0)
+            {
+                Paper.Copy();
+            }
+        }
+
+        private void MenuItem_Paste(object sender, EventArgs e)
+        {
+            if (Clipboard.GetDataObject().GetDataPresent(DataFormats.Text) == true)
+            {
+                if (Paper.SelectionLength > 0)
+                {
+                    Paper.SelectionStart = Paper.SelectionStart + Paper.SelectionLength;
+                }
+                Paper.Paste();
+            }
+        }
+
+        private void MenuItem_Cut(object sender, RoutedEventArgs e)
+        {
+            if (Paper.SelectionLength > 0)
+            {
+                Paper.Cut();
+            }
         }
     }
 }
